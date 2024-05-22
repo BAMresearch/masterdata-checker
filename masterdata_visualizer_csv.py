41fd503aa5bd48eda009bcf1a999315755654063
Madariaga <carlos.madariaga@bam.de>
2024-05-22 17:12:25 +0200

    Added number of types and properties by object type

--- masterdata_visualizer_csv.py
+++ masterdata_visualizer_csv.py
@@ -22,18 +22,20 @@ current_date = datetime.now().strftime("%d-%m-%Y")
 
 info = [instance, current_date]
 
-
-masterdata_headers = ["SPACES", "PROJECTS", "EXPERIMENT TYPES", "OBJECT TYPES", "MATERIAL TYPES", "DATASET TYPES", "VOCABULARIES", "PLUGINS"]
-
 # Fetch data from the server
 spaces = [space for space in o.get_spaces()]
 projects = [project.code for project in o.get_projects()]
 experiment_types = [exp for exp in o.get_experiment_types()]
-object_types = [obj for obj in o.get_object_types()]
+object_types = [obj for obj in o.get_object_types() if obj.code != "UNKNOWN"]
 material_types = [material for material in o.get_material_types()]
 dataset_types = [dataset for dataset in o.get_dataset_types()]
 vocabs = [vocab.code for vocab in o.get_vocabularies()]
 plugins = [plug.name for plug in o.get_plugins()]
+
+masterdata_headers = [f"SPACES ({len(spaces)})", f"PROJECTS ({len(projects)})", f"EXPERIMENT TYPES ({len(experiment_types)})", 
+                      f"OBJECT TYPES ({len(object_types)})", f"DATASET TYPES ({len(dataset_types)})",
+                      f"VOCABULARIES ({len(vocabs)})", f"PLUGINS ({len(plugins)})", f"MATERIAL TYPES ({len(material_types)})"]
+
 
 # Combine master data into a list of lists
 masterdata = [
@@ -41,10 +43,10 @@ masterdata = [
     projects,
     experiment_types,
     object_types,
-    material_types,
     dataset_types,
     vocabs,
-    plugins
+    plugins,
+    material_types
 ]
 # Directory name based on instance
 directory = f"{instance}_data"
@@ -92,5 +94,28 @@ with open(filename, mode='w', newline='') as file:
         
     # Write another header row with the content of object_types horizontally
     writer.writerow(object_types)
+    
+    props_by_obj = []
+    
+    for obj in object_types:
+        if obj.code == "UNKNOWN":
+            continue
+        props = []
+        for prop in obj.get_property_assignments():
+            props.append(f"{prop.code} ({str(prop.dataType).lower()})")
+        props_by_obj.append(props)
+        
+    # Determine the maximum length of the object properties
+    max_length_props = max(len(properties) for properties in props_by_obj)
+    
+    # Write the master data vertically
+    for i in range(max_length_props):
+        row = []
+        for prop_list in props_by_obj:
+            if i < len(prop_list):
+                row.append(prop_list[i])
+            else:
+                row.append("")  # Append empty string if the list is shorter
+        writer.writerow(row)
     
 print(f"CSV file '{filename}' has been created.")
