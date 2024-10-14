58ca5bbf730e4c97fcbdf628fab2d1276b959bb8
Madariaga <carlos.madariaga@bam.de>
2024-10-14 15:14:56 +0200

    Added functionality for entity checks vs openbis

--- django_checker/masterdata_checker/myapp/views.py
+++ django_checker/masterdata_checker/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from myapp.utils import name_checker, content_checker
+from pybis import Openbis
+from myapp.utils import name_checker, content_checker, entity_checker
 import logging
 
 # Get an instance of the logger for the app (replace 'myapp' with your app name)
@@ -14,14 +15,18 @@ def homepage(request):
             if uploaded_file.name.endswith(('.xls', '.xlsx')):
                 try:
                     
+                    url = f"https://devel.datastore.bam.de/"
+                    o = Openbis(url)
+                    o.login("cmadaria", "psswd", save_token=True)
+                    
                     file_name = uploaded_file.name
 
                     result_name = name_checker(file_name)[0]
                     name_ok = name_checker(file_name)[1]
                     result_content = str(content_checker(uploaded_file, name_ok))
-                    result_entity = ""#str(entity_checker(file_path, o))
+                    result_entity = str(entity_checker(uploaded_file, o))
                     logger.info(f"Type {type(file_name)} of file {file_name}")
-                    result_format = "CHECKED NAME:" + "\n-------------\n" + result_name + "\n" + "\nCHECKED CONTENT:" + "\n-------------\n" + result_content + "\n" + "\nCHECKED ENTITY" + "\n-------------\n" + result_entity
+                    result_format = "CHECKED NAME:" + "\n----------------------------\n" + result_name + "\n" + "\nCHECKED CONTENT:" + "\n----------------------------\n" + result_content + "\n" + "\nCHECKED ENTITY" + "\n----------------------------\n" + result_entity
 
 
                     context["result"] = result_format
