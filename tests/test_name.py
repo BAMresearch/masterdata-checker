9c7f17d36cd102f4d95bd329a789d4a1bd4e5e30
jpizarro <jose.pizarro-blanco@bam.de>
2024-11-19 15:57:37 +0000

    Restructuring the project

---
+++ tests/test_name.py
@@ -0,0 +1,21 @@
+import pytest
+
+from masterdata_checker.name import name_checker
+
+
+# @pytest.mark.parametrize(
+#     'file_path, result',
+#     [
+#         ('whatever-filepath', ['whatever-result-we-expert', True]),
+#         ('another-filepath', ['another-result-we-expert', False]),
+#     ]
+# )
+# def test_name_checker(filepath: str, result: list[str, bool]):
+def test_name_checker():
+    """
+    Tests the `name_checker` function defined in the `name.py` module.
+    """
+
+    # result = name_checker(file_path=file_path)
+
+    assert True  # dummy test as a placeholder
