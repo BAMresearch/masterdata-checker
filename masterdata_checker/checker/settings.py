521fac2c57c8d101a994b9eb754ff8b1faf99245
jpizarro <jose.pizarro-blanco@bam.de>
2025-06-10 10:27:51 +0200

    Added staticfiles for when DEBUG=False
    Added whitenoise for when DEBUG=False
    
    Fixed WSGI_APPLICATION in settings

--- masterdata_checker/checker/settings.py
+++ masterdata_checker/checker/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -83,7 +84,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "checker.wsgi.application"
+WSGI_APPLICATION = "masterdata_checker.checker.wsgi.application"
 
 
 # Database
@@ -147,7 +148,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "app/static"]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
