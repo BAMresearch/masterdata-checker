bc5daeff9702c2b5a6e79e3ccd83eee8224efb40
jpizarro <jose.pizarro-blanco@bam.de>
2025-06-11 10:35:31 +0200

    Removed login page from app

--- masterdata_checker/app/urls.py
+++ masterdata_checker/app/urls.py
@@ -4,6 +4,4 @@ from . import views
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
-    path("login/", views.login, name="login"),
-    path("logout/", views.logout_view, name="logout"),
 ]
